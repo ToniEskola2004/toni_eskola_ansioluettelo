7a876069d060525dad51c5f338c28b934dd62c13
Toni Eskola <toni.eskola992@gmail.com>
2024-06-24 14:19:13 +0300

    jquery korjaus

--- js/contactform.js
+++ js/contactform.js
@@ -1,24 +1,24 @@
 jQuery(document).ready(function ($) {
-    var $root = ('html, body');
+    var $root = $('html, body');
 
     $(".contact-form").submit(function (event) {
-
-        //stop form from submitting normally
+        // Stop form from submitting normally
         event.preventDefault();
 
         // Serialize submitting form and get action
         var $form = $(this),
             url = $form.attr("action");
+
         // Send the data using post
-        var posting = $.post(url, $form-serialize());
+        var posting = $.post(url, $form.serialize());
 
         // Put the results in a div
-        posting.done(function(data) {
+        posting.done(function (data) {
             var response = $.parseJSON(data),
                 target = $('#status-messages');
-            
-            //Add success/error classes
-            if(response.status == 1) {
+
+            // Add success/error classes
+            if (response.status == 1) {
                 target.removeClass('error');
                 target.addClass('success');
             } else if (response.status == 0) {
@@ -26,9 +26,8 @@ jQuery(document).ready(function ($) {
                 target.addClass('error');
             }
 
-            //append message
-            target.empty.append(response.message).hide().fadeIn(400);
+            // Append message
+            target.empty().append(response.message).hide().fadeIn(400);
         });
     });
-
 });
