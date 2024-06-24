03480f4f7c1316180bbbaa5206cf661ec8831f85
Toni Eskola <toni.eskola992@gmail.com>
2024-06-24 13:57:06 +0300

    contact form jquery

--- js/contactform.js
+++ js/contactform.js
@@ -1,3 +1,34 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     var $root = ('html, body');
-})
+
+    $(".contact-form").submit(function (event) {
+
+        //stop form from submitting normally
+        event.preventDefault();
+
+        // Serialize submitting form and get action
+        var $form = $(this),
+            url = $form.attr("action");
+        // Send the data using post
+        var posting = $.post(url, $form-serialize());
+
+        // Put the results in a div
+        posting.done(function(data) {
+            var response = $.parseJSON(data),
+                target = $('#status-messages');
+            
+            //Add success/error classes
+            if(response.status == 1) {
+                target.removeClass('error');
+                target.addClass('success');
+            } else if (response.status == 0) {
+                target.removeClass('success');
+                target.addClass('error');
+            }
+
+            //append message
+            target.empty.append(response.message).hide().fadeIn(400);
+        });
+    });
+
+});
