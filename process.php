fc48dc62c994f4bf485f2c2145f93b6eb13e2135
Toni Eskola <toni.eskola992@gmail.com>
2024-06-24 14:14:27 +0300

    contact form php korjaus

--- process.php
+++ process.php
@@ -3,46 +3,44 @@
 $sendto = 'toni.eskola992@gmail.com';
 $subject = 'Uusi viesti';
 
-if ( !empty( $_POST ) ) {
-    //Whitelist
+if (!empty($_POST)) {
+    // Whitelist
     $name = $_POST['name'];
-    $form = $_POST['email'];
-    $message = $_POST['messsage'];
+    $from = $_POST['email'];
+    $message = $_POST['message'];
     $honeypot = $_POST['URL'];
 
-    //check hopneypot
-    if( !empty( $honeypot ) ) {
-        echo json_encode(array('status'=>0 'message'=>'Tapahtui virhe'));
-
+    // Check honeypot
+    if (!empty($honeypot)) {
+        echo json_encode(array('status' => 0, 'message' => 'Tapahtui virhe'));
         die();
     }
 
-    //check for empty values
-if ( empty($name) || empty( $from ) || empty( $message )) {
-    echo json_encode (array('status'=>0 'message'=>'Täytä kaikki kentät'));
-
+    // Check for empty values
+    if (empty($name) || empty($from) || empty($message)) {
+        echo json_encode(array('status' => 0, 'message' => 'Täytä kaikki kentät'));
         die();
     }
 
-    // check for valid email
-    $from = filter_var($from, FILTER_VALID_EMAIL);
-
-    if ( !$from ) {
-        echo json_encode(array('status' =>0, 'message'=>'Ei ole oikea sähköposti'));
-
+    // Check for valid email
+    $from = filter_var($from, FILTER_VALIDATE_EMAIL);
+    if (!$from) {
+        echo json_encode(array('status' => 0, 'message' => 'Ei ole oikea sähköposti'));
         die();
     }
 
     // Send email
-    $headers = sprintf('From: %s', $from) ."\r\n";
-    $headers .= sprintf('Reply-To: %s', $from) ."\r\n";
+    $headers = sprintf('From: %s', $from) . "\r\n";
+    $headers .= sprintf('Reply-To: %s', $from) . "\r\n";
     $headers .= sprintf('X-Mailer: PHP/%s', phpversion());
 
-    if( mail($sendto, $subject, $message, $headers) ) {
-        echo json_encode(array('status' =>1, 'message'=>'Viesti lähetetty onnituneesti'));
-
+    if (mail($sendto, $subject, $message, $headers)) {
+        echo json_encode(array('status' => 1, 'message' => 'Viesti lähetetty onnistuneesti'));
         die();
     }
 
-    echo json_encode(array('status' =>0, 'message'=>'Lähetys ei onnisutnut. Yritä uudelleen'));
-} else 'No data'
+    echo json_encode(array('status' => 0, 'message' => 'Lähetys ei onnistunut. Yritä uudelleen'));
+} else {
+    echo 'No data';
+}
+?>
